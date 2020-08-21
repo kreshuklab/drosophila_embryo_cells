ccfddd8404c45c50953dca9421c4ae81c66e8612
vzinche <valentyna.zinchenko@embl.de>
2020-08-21 17:24:25 +0200

    simple convnet

---
+++ scripts/models.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn as nn
+
+
+class SimpleConvNet(nn.Module):
+    def __init__(self, num_classes, num_layers=4, in_channels=1,
+                 init_num_fmaps=8, fmap_growth=2, pool='avg', fc_size=20):
+        super().__init__()
+        self.in_channels = in_channels
+        self.channels = [in_channels, ] + [init_num_fmaps * fmap_growth**i
+                                           for i in range(num_layers)]
+        self.conv_layers = self.get_conv_layers(num_layers)
+        assert pool in ['avg', 'max']
+        self.pool = nn.AdaptiveAvgPool2d(1) if pool == 'avg' else nn.AdaptiveMaxPool3d(1)
+        self.fc1 = nn.Linear(self.channels[-1], fc_size)
+        self.fc2 = nn.Linear(fc_size, num_classes)
+
+    def get_conv_layers(self, num):
+        layers = []
+        for i in range(num):
+            in_ch, out_ch = self.channels[i], self.channels[i + 1]
+            layer = nn.Sequential(nn.Conv2d(in_ch, out_ch, kernel_size=3, padding=1),
+                                  nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+            layers.append(layer)
+        return nn.ModuleList(layers)
+
+    def forward(self, x):
+        for conv_layer in self.conv_layers:
+            x = conv_layer(x)
+        x = self.pool(x)
+        x = torch.flatten(x, 1)
+        print(x.shape)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = torch.squeeze(x, dim=1)
+        return x
