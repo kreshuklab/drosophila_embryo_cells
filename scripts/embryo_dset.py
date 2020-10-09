81934b8f00df7a300c3a20b9cd9b5691841b6e0c
vzinche <valentyna.zinchenko@embl.de>
2020-10-09 11:45:55 +0200

    added offsets and myo neighborhoud

--- scripts/embryo_dset.py
+++ scripts/embryo_dset.py
@@ -57,21 +57,26 @@ class ClassMyoDataset(ClassSegmDataset):
 
 
 class ClassAlignSegmDataset(EmbryoDataset):
-    def __init__(self, indices, *args, **kwargs):
+    def __init__(self, indices, offset=None, **kwargs):
         # we expect [idx, tf]
         assert indices.shape[-1] == 2
         self.idx2tf = {idx: tf for idx, tf in indices}
-        super().__init__(*args, indices=indices[:, 0], **kwargs)
-        self.bbs = self.get_bbs(self.segm_data)
+        self.offset = offset
+        super().__init__(indices=indices[:, 0], **kwargs)
+        self.bbs = self.get_bbs(self.segm_data, offset=offset)
         rows_present = set([self.class_labels[i] for i in indices[:, 0]])
         # we need to make sure we have all classes present
         print("The number of samples per class is \n",
               np.unique([self.class_labels[i] for i in indices[:, 0]], return_counts=True))
         assert all(i in rows_present for i in ALL_ROWS)
 
-    def get_bbs(self, segm_volume):
+    def get_bbs(self, segm_volume, offset=None):
         idx2bb = {idx : find_objects(segm_volume[tf] == idx)[0]
                   for idx, tf in self.idx2tf.items()}
+        if offset is not None:
+            idx2bb_off = {idx : tuple([slice(max(0, sl.start - offset), sl.stop + offset) for sl in bb])
+                          for idx, bb in idx2bb.items()}
+            return idx2bb_off
         return idx2bb
 
     def __getitem__(self, idx):
@@ -82,12 +87,17 @@ class ClassAlignSegmDataset(EmbryoDataset):
 
 
 class ClassAlignMyoDataset(ClassAlignSegmDataset):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, add_cell_mask=False, **kwargs):
+        self.add_cell_mask = add_cell_mask
         super().__init__(*args, **kwargs)
 
     def __getitem__(self, idx):
         cell_id = self.indices[idx]
         cell_class = self.class_labels[cell_id]
         cell_mask = self.segm_data[self.idx2tf[cell_id]][self.bbs[cell_id]] == cell_id
-        myo_mask = self.myosin_data[self.idx2tf[cell_id]][self.bbs[cell_id]] * cell_mask
-        return self.transforms(myo_mask), self.norm_label(cell_class)
+        myo_data = self.myosin_data[self.idx2tf[cell_id]][self.bbs[cell_id]]
+        if self.offset is None:
+            myo_data = myo_data * cell_mask
+        if self.add_cell_mask:
+            myo_data = np.stack([myo_data, cell_mask])
+        return self.transforms(myo_data), self.norm_label(cell_class)
